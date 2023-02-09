ebb908ac24f1ab66ed06db08d531214619a718a5
Michael Novotny <manovotny@gmail.com>
2023-02-08 23:11:10 -0600

    Adds `suppressHydrationWarning`.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Providers } from './providers';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <Providers>
         <body className="bg-white dark:bg-black">
           <main className="min-h-screen overflow-hidden text-black dark:text-white">
